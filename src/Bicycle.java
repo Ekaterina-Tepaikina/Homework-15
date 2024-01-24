1ef05b3aed083ba4aa1ab4625e96b7b4d76eb1ed
MaximTep <tepajkin01@mail.ru>
2024-01-24 18:21:24 +0300

    Внесены правки

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -3,4 +3,14 @@ public class Bicycle extends Vehicle {
     public Bicycle (String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void service() {
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+        }
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,18 +1,10 @@
-public class Vehicle {
+public abstract class Vehicle {
     private String modelName;
     private int wheelsCount;
 
     public Vehicle (String modelName, int wheelsCount) {
         this.modelName = modelName;
         this.wheelsCount = wheelsCount;
-    }
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
     }
 
     public String getModelName() {
@@ -34,4 +26,6 @@ public class Vehicle {
             this.wheelsCount = wheelsCount;
         }
     }
+
+    public abstract void service();
 }
